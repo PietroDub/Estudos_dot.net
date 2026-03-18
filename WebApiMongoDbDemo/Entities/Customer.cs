7a64fc8d5c4be8dc0be0a8ed051397a5486e262c
PietroDub <pieti453@gmail.com>
2026-03-18 09:03:55 -0300

    Adicionar arquivos de projeto.

---
+++ WebApiMongoDbDemo/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApiMongoDbDemo.Entities
+{
+    public class Customer
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("_nome"), BsonRepresentation(BsonType.String)]
+        public string? CustomerName { get; set; }
+        [BsonElement("_email"), BsonRepresentation(BsonType.String)]
+        public string? Email { get; set; }
+    }
+}
